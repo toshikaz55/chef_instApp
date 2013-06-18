61d93b48b0214f0d062532ec0a30d2ded34fbf9e
toshikaz55 <toshikaz@gmail.com>
2013-06-18 10:18:51 +0900

    add yum repo epel

--- site-cookbooks/instApp/recipes/instApp.rb
+++ site-cookbooks/instApp/recipes/instApp.rb
@@ -7,10 +7,27 @@
 # All rights reserved - Do Not Redistribute
 #
 
+bash "instal yum.repo epel" do
+  user "root"
+  cwd "/tmp"
+
+  code <<-EOC
+    rpm -Uvh "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+  EOC
+  not_if { ::File.exists?("/etc/yum.repos.d/epel.repo") }
+end
+
+
 log "install zsh emacs w3m lynx"
-%w{ zsh emacs ntp sqlite-devel git ruby gem tree w3m lynx }.each do |pkg|
+%w{ man zsh emacs ntp sqlite-devel git tmux tree w3m lynx }.each do |pkg|
   log pkg
   package pkg do
     action :install
   end
 end
+
+#rpm_package "http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm" do
+#  action :install
+#end
+
+ 
